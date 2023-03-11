6334853830bb13ca12f0848a85586974f1d75269
Naz Demirsoy <75198796+nazdemirsoyy@users.noreply.github.com>
2023-03-11 12:29:26 +0300

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,28 @@
+print("Bana \"Bugün ne yapıyorsun\" dedi")
+print()
+print("hey\tnasılsın")
+print()
+print("5" + "4")
+print()
+print(4 * "hey")
+print()
+print(len("hey!")) #4
+print()
+
+
+print("String Indexing")
+#String Indexing
+name = "Deniz"
+print(name[1])
+print(name[-1]) #z
+#print(name[-6]) #IndexError: string index out of range
+
+print()
+print("String Slice")
+print(name[0:3]) #Den
+print(name[0:10:2]) #Dnz
+print(name[::-1])
+
+
+
+
