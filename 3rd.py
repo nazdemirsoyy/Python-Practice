da394d120ddb47232ebbfdde14bceb91714e3663
Naz Demirsoy <75198796+nazdemirsoyy@users.noreply.github.com>
2023-03-16 17:18:28 +0300

    Update 3rd.py

--- 3rd.py
+++ 3rd.py
@@ -12,6 +12,44 @@ while c0 != 1:
     print(c0)
 
 
+## LIST
+list = ["1", "2", "3"]
+# print(list)
+list.append("5")
+# print(list) # ['1', '2', '3', '5']
+
+list.extend([4, 5, 6])
+for i in range(7, 9):
+    list.append(i)
+#print(list)
+
+any_list = [1,2,4,5,6,9]
+any_list.pop()
+#print(any_list)
+
+beatles= []
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+
+#use for loop to append
+for i in range(2):
+    user_name= input("Enter a user name: ")
+    beatles.append(user_name)
+
+print(beatles) # See the changes before delition
+
+
+# use the del to remove Stu Sutcliffe and Pete Best from the list
+del beatles[3]
+del beatles[3]
+
+print(beatles)
+
+# Use the insert() method to add Ringo Starr to the beggining of the list
+beatles.insert(0,"Ringo Starr")
+
+print(beatles)
 
  
 
